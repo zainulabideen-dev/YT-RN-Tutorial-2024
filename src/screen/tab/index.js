a6c3ef99281d9f51b3dcd4324ad091e9c4f0677a
zainulabideendev <zainulabideendev@gmail.com>
2024-06-07 20:00:35 +0500

    vector icons tab view adjustments

--- src/screen/tab/index.js
+++ src/screen/tab/index.js
@@ -1,6 +1,7 @@
 import {View, Text, SafeAreaView, useWindowDimensions} from 'react-native';
 import React, {useState} from 'react';
-import {SceneMap, TabView} from 'react-native-tab-view';
+import {SceneMap, TabBar, TabView} from 'react-native-tab-view';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 const FirstRoute = () => <View style={{flex: 1, backgroundColor: '#ff4081'}} />;
 
@@ -22,9 +23,30 @@ export default function TabScreen() {
     {key: 'second', title: 'Second'},
   ]);
 
+  const renderTabBar = props => (
+    <TabBar
+      {...props}
+      indicatorStyle={{backgroundColor: 'white'}}
+      style={{backgroundColor: 'orange'}}
+      renderLabel={({route, focused, color}) => (
+        <Text style={{color: focused ? 'black' : 'red', margin: 8}}>
+          {route.title}
+        </Text>
+      )}
+      renderIcon={({route, focused, color}) =>
+        route.title == 'First' ? (
+          <AntDesign name={'home'} color={color} size={20} />
+        ) : (
+          <AntDesign name={'user'} color={color} size={20} />
+        )
+      }
+    />
+  );
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <TabView
+        renderTabBar={renderTabBar}
         navigationState={{index, routes}}
         renderScene={renderScene}
         onIndexChange={setIndex}
